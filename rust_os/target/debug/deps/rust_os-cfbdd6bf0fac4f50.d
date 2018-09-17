830b81118d5b58b9fbfb810845c064422b467920
MichalObi <ndwz123@gmail.com>
2018-09-17 20:21:02 +0200

    Remove target folder

--- rust_os/target/debug/deps/rust_os-cfbdd6bf0fac4f50.d
+++
@@ -1,5 +0,0 @@
-/home/michal/Projects/rust-os/rust_os/target/debug/deps/rust_os-cfbdd6bf0fac4f50: src/main.rs
-
-/home/michal/Projects/rust-os/rust_os/target/debug/deps/rust_os-cfbdd6bf0fac4f50.d: src/main.rs
-
-src/main.rs:
--- rust_os/target/debug/deps/rust_os-d9d8302084f5b91f.d
+++
@@ -1,5 +0,0 @@
-/home/michal/Projects/rust-os/rust_os/target/debug/deps/rust_os-d9d8302084f5b91f: src/main.rs
-
-/home/michal/Projects/rust-os/rust_os/target/debug/deps/rust_os-d9d8302084f5b91f.d: src/main.rs
-
-src/main.rs:
